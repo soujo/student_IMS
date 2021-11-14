58014e1ec8a5977d1f785199512643b9ca65085f
Soujanya Hazra <soujanya0105@gmail.com>
2021-11-15 01:58:32 +0530

    initial commit

---
+++ static/js/student.js
@@ -0,0 +1,21 @@
+const dateTime= document.querySelector(".date__time");
+const flagCross=document.querySelector(".flag__cross");
+const flag=document.getElementById("flag");
+
+let today=new Date();
+
+let time = today.toLocaleString('en-IN', {  
+    weekday:"long",
+    day:"numeric",
+    month:"long",
+    year:"numeric",
+    hour:"2-digit",
+    hour12:true,
+    minute:"numeric"
+});
+dateTime.innerText=`${time}`;
+
+flagCross.addEventListener("click",function (){
+    flag.classList.remove("flag"); 
+    flag.classList.add("off");
+});
