8e521a9491e266ee97e10a1755ebddbaa6394cd3
Soujanya Hazra <soujanya0105@gmail.com>
2021-11-19 02:00:06 +0530

    initial commit

---
+++ static/js/achievements.js
@@ -0,0 +1,23 @@
+const dateTime= document.querySelector(".date__time");
+const flagCross=document.querySelector(".flag__cross");
+const flag=document.getElementById("flag");
+const achievements= document.querySelector(".achievements"); 
+let today=new Date();
+
+let time = today.toLocaleString('en-IN', {  
+    weekday:"long",
+    day:"numeric",
+    month:"long",
+    year:"numeric",
+    hour:"2-digit",
+    hour12:true,
+    minute:"numeric"
+});
+dateTime.innerText=`${time}`;
+
+flagCross.addEventListener("click",function (){
+    flag.classList.remove("flag");
+    flag.classList.add("off");
+});
+
+achievements.style.backgroundColor = "rgb(0, 60, 128)";
