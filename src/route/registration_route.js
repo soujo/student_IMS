4eaa75174b62f1870a4e3674265b837dd24fd2e3
Soujanya Hazra <soujanya0105@gmail.com>
2021-11-29 14:25:09 +0530

    post-request added

--- src/route/registration_route.js
+++ src/route/registration_route.js
@@ -1,10 +1,41 @@
 const express = require("express");
 const Router = express.Router();
+const Register = require("../models/userRegistration");
 
 Router.route("/registration")
-    .get(async (req, res) => { 
+    .get(async (req, res) => {
         res.status(200).render("registration.pug");
     })
-    
+    .post(async (req, res) => {
 
-module.exports=Router;
+        try {
+            const password = req.body.password;
+            const confirmPassword = req.body.confirmPassword;
+
+            if (confirmPassword === password) {
+                const students = new Register({
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    email: req.body.email,
+                    phone: req.body.phone,
+                    password: password,
+                    confirmPassword: confirmPassword,
+                    roll: req.body.roll,
+                    gender: req.body.gender
+                });
+
+                const registered = await students.save();
+                res.status(201).send("Registration is successful");
+            }
+            else {
+                res.send("Password are not matching");
+            }
+
+        }
+        catch (err) {
+            res.status(400).send(err);
+            console.log(err);
+        }
+    })
+
+module.exports = Router;
