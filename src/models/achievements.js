0c60f0f3b31ea52bd975d0e82e4ac3303e95463a
soujanya hazra <soujanya0105@gmail.com>
2021-12-11 22:38:54 +0530

    student-achievement schema added

---
+++ src/models/achievements.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const achievementSchema = new mongoose.Schema(
+    {
+        hobbies: {
+            type: String,
+            uppercase: true
+        },
+        sports: {
+            type: String,
+            uppercase: true
+        },
+        language: {
+            type: String,
+            uppercase: true
+        },
+        clubs: {
+            type: Array
+        },
+        roll: {
+            type: Number
+        },
+        achievement: [ 
+            {
+                category: {
+                    type: String,
+                    uppercase: true
+                },
+                achievements_item: {
+                    type: String,
+                    uppercase: true
+                },
+                description_text: {
+                    type: String
+                }
+            }
+        ],
+        edit:{
+            type:String
+        }
+
+    },
+    { typeKey: 'type' }
+);
+
+const Achievement = new mongoose.model("Achievement", achievementSchema);
+
+module.exports = Achievement;
