c330d6ff259a9307dddc6153f28efad4c94431f0
soujanya hazra <soujanya0105@gmail.com>
2021-12-12 11:23:17 +0530

    student achievement-edit route added

---
+++ src/route/achievementEdit_route.js
@@ -0,0 +1,141 @@
+const express = require("express");
+const Router = express.Router();
+const rollNumber = require("../app");
+const Register = require("../models/userRegistration");
+const Achievement = require("../models/achievements");
+
+Router.route("/achievementsEdit")
+    .get( async (req, res) => {
+
+        try {
+            const userRoll = await Register.findOne({ roll: rollNumber.roll });
+
+            const firstName = userRoll?.firstName;
+            const lastName = userRoll?.lastName;
+            const params = {
+                "content": "Achievements",
+                "firstName": firstName,
+                "lastName": lastName
+            };
+
+            res.status(200).render("achievementsEdit.pug", params);
+        }
+        catch (err) {
+            res.status(400).send(err);
+            console.log(err);
+        }
+    })
+    .post(async (req, res) => {
+
+
+        const achievementRoll = await Achievement.findOne({ roll: rollNumber.roll });
+        const edit = achievementRoll?.edit;
+
+        if (edit == undefined) {
+
+            try {
+
+                let achieveObjects = [];
+                for (let i = 0; i < req.body.category.length; i++) {
+                    achieveObjects.push({
+                        category: req.body.category[i],
+                        achievements_item: req.body.achievements_item[i],
+                        description_text: req.body.description_text[i],
+                    });
+                }
+                const achievements = new Achievement({
+                    hobbies: req.body.hobbies,
+                    language: req.body.language,
+                    sports: req.body.sports,
+                    clubs: [
+                        req.body.DSC,
+                        req.body.SAC,
+                        req.body.riyaz,
+                        req.body.elysium,
+                        req.body.chitrank,
+                        req.body.infinitio,
+                        req.body.litmus,
+                        req.body.ecell,
+                        req.body.quiz,
+                        req.body.shutterburg,
+                        req.body.robotics
+                    ],
+                    roll: rollNumber.roll,
+                    achievement: achieveObjects,
+                    edit: "firstTime"
+                });
+                const achievementsSubmitted = await achievements.save();
+                res.status(200).redirect("/student/achievements");
+
+
+            }
+            catch (err) {
+                res.status(400).send(err);
+                console.log(err);
+            }
+
+        }
+        else {
+
+            const id = achievementRoll?.id;
+            const updateDocuments = async (_id) => {
+                try {
+
+                    let achieveObjects = [];
+                    for (let i = 0; i < req.body.category.length; i++) {
+                        achieveObjects.push({
+                            category: req.body.category[i],
+                            achievements_item: req.body.achievements_item[i],
+                            description_text: req.body.description_text[i],
+                        });
+                    }
+
+                    let j = 1;
+                    let update = await Achievement.findByIdAndUpdate(
+                        { _id },
+                        {
+                            $set: {
+                                hobbies: req.body.hobbies,
+                                language: req.body.language,
+                                sports: req.body.sports,
+                                clubs: [
+                                    req.body.DSC,
+                                    req.body.SAC,
+                                    req.body.riyaz,
+                                    req.body.elysium,
+                                    req.body.chitrank,
+                                    req.body.infinitio,
+                                    req.body.litmus,
+                                    req.body.ecell,
+                                    req.body.quiz,
+                                    req.body.shutterburg,
+                                    req.body.robotics
+                                ],
+                                roll: rollNumber.roll,
+                                achievement: achieveObjects,
+                                edit: `updated-${j++}`
+                            }
+                        },
+                        {
+                            new: true,
+                            useFindAndModify: false
+                        }
+                    );
+                    const updated = await update.save();
+                    res.status(200).redirect("/student/achievements");
+
+                }
+                catch (err) {
+                    res.status(400).send(err);
+                    console.log(err);
+                }
+
+            };
+
+            updateDocuments(id);
+        }
+
+    })
+
+
+module.exports = Router;
