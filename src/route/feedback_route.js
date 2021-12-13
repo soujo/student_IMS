81c7bd9ac896f9557a0b7a6446fb9e0f191ab4ed
soujanya hazra <soujanya0105@gmail.com>
2021-12-14 00:26:54 +0530

    student-feedback route added

---
+++ src/route/feedback_route.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const Router = express.Router();
+const rollNumber = require("../app");
+const Register = require("../models/userRegistration");
+const PersonalInfo = require("../models/personalInfo");;
+const Feedback=require("../models/feedback");
+
+
+Router.route("/feedback")
+    .get(async (req, res) => { 
+        try {
+
+            const userRoll = await Register.findOne({ roll: rollNumber.roll });
+
+            const firstName = userRoll?.firstName;
+            const lastName = userRoll?.lastName;
+            const email = userRoll?.email;
+
+            const personalInfoRoll = await PersonalInfo.findOne({ roll: rollNumber.roll });
+            const branch = personalInfoRoll?.branch;
+            const param = {
+                "content":"Feedback",
+                "firstName": firstName,
+                "lastName": lastName,
+                "email": email,
+                "dept": branch,
+                "roll": rollNumber.roll
+            };
+
+            res.status(200).render("feedback.pug", param);
+        }
+        catch (err) {
+            res.status(400).send(err);
+            console.log(err);
+        }
+    })
+    .post(async (req, res) => {
+        try {
+
+            const Feedbacks = new Feedback({
+                name: req.body.name,
+                email: req.body.email,
+                dept: req.body.dept,
+                roll: req.body.roll,
+                msg: req.body.msg
+            });
+            const feedbackSubmitted = await Feedbacks.save();
+            res.status(200).redirect("/student/feedback");
+        }
+        catch (err) {
+
+            res.status(400).send(err);
+            console.log(err);
+        }
+    });
+
+
+module.exports = Router;
