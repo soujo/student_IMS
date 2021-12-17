5257ae151cb5fb6e4c33282287f6a78b0e414d03
soujanya hazra <soujanya0105@gmail.com>
2021-12-17 22:55:17 +0530

    student club-route added

---
+++ src/route/clubs_route.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const Router = express.Router();
+const rollNumber = require("../app");
+const Register = require("../models/userRegistration");
+
+Router.route("/clubs")
+    .get(async (req, res) => {
+        try {
+            const userRoll = await Register.findOne({ roll:rollNumber.roll });
+    
+            const firstName = userRoll?.firstName;
+            const lastName = userRoll?.lastName;
+            const image = `../static/uploads/${rollNumber.roll}.jpeg`;
+            const param = {
+                "content":"Clubs Info",
+                "firstName": firstName,
+                "lastName": lastName,
+                "image":image
+            };
+            res.status(200).render("clubs.pug", param);
+        }
+        catch (err) {
+            res.status(400).send(err);
+            console.log(err);
+        }
+    })
+
+
+module.exports = Router;
