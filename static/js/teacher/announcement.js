c755461428da00446e6b2e469641522e0469d0bd
soujanya hazra <soujanya0105@gmail.com>
2021-12-23 11:38:22 +0530

    initial commit

---
+++ static/js/teacher/announcement.js
@@ -0,0 +1,24 @@
+const dateTime= document.querySelector(".date__time");
+const flagCross=document.querySelector(".flag__cross");
+const flag=document.getElementById("flag");
+const announcement= document.querySelector(".announcement");
+
+let today=new Date();
+
+let time = today.toLocaleString('en-IN', {  
+    weekday:"long",
+    day:"numeric",
+    month:"long",
+    year:"numeric",
+    hour:"2-digit",
+    hour12:true,
+    minute:"numeric"
+});
+dateTime.innerText=`${time}`;
+
+flagCross.addEventListener("click",function (){
+    flag.classList.remove("flag");
+    flag.classList.add("off");
+});
+
+announcement.style.backgroundColor = "rgb(0, 60, 128)";
