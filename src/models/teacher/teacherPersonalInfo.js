67228cdb11b6158fdb33cace224b0f51ea3259ae
soujanya hazra <soujanya0105@gmail.com>
2022-01-01 21:24:30 +0530

    teacher personal-info schema added

---
+++ src/models/teacher/teacherPersonalInfo.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+
+const TpersonalInfoSchema = new mongoose.Schema({
+    name:{
+        type:String
+    },
+    regNum:{
+        type:String
+    },
+    batch:{
+        type:String
+    },
+    dept:{
+        type:String
+    },
+    sem:{
+        type:String
+    },
+    sub:{
+        type:String
+    },
+    dob:{
+        type:String
+    },
+    category:{
+        type:String
+    },
+    gender:{
+        type:String
+    },
+    bloodGroup:{
+        type:String
+    },
+    aadhar:{
+        type:String
+    },
+    fatherName:{
+        type:String
+    },
+    motherName:{
+        type:String
+    },
+    email:{
+        type:String
+    },
+    phone:{
+        type:String
+    },
+    address:{
+        type:String
+    }
+});
+
+const TeacherPersonalInfo = new mongoose.model("TeacherPersonalInfo",TpersonalInfoSchema);
+
+module.exports= TeacherPersonalInfo;
