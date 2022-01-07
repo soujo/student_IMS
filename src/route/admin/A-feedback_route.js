54ae3681edf5984e582ecf17c6b418c1959416e2
soujanya hazra <soujanya0105@gmail.com>
2022-01-07 21:00:34 +0530

    dynamic data fetched

--- src/route/admin/A-feedback_route.js
+++ src/route/admin/A-feedback_route.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const Router = express.Router();
+const Feedback = require("../../models/feedback");
 
 Router.route("/feedbacks")
     .get(async (req, res) => {
         try {
+            const feedbacks = await Feedback.find();
             const param = {
                 "content": "Feedbacks",
+                "feedbackArray": feedbacks,
+                "msg": "No feedback by anyone"
             }
             res.status(200).render("admin/A-feedback.pug", param);
 
