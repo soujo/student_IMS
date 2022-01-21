67e4d05bdff89b1b7b699470eae941a1a9d77294
soujanya hazra <soujanya0105@gmail.com>
2022-01-22 02:09:15 +0530

    student login route added

---
+++ src/route/login_route.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const Router = express.Router();
+const Register = require("../models/userRegistration");
+const bcrypt = require("bcryptjs");
+
+Router.route("/studentLogin")
+
+    .get((req, res) => {
+        res.status(200).render("login.pug",
+            {
+                msg: req.flash("login-err"),
+                login_msg: req.flash("reg-success")
+            });
+    })
+    .post(async (req, res) => {
+        try {
+            const roll = req.body.roll;
+            const password = req.body.password;
+
+            const userRoll = await Register.findOne({ roll });
+
+            const isMatch = await bcrypt.compare(password, userRoll?.password);
+            if (isMatch) {
+                res.status(200).redirect("/student/homepage");
+            }
+            else {
+                req.flash("login-err", "Invalid Credentials");
+                res.redirect("/home/studentLogin");
+            }
+        }
+        catch (err) {
+            console.log(err);
+            req.flash("login-err", "Roll number is not registered in our system ! Kindly register");
+            res.redirect("/home/studentLogin");
+        }
+    });
+
+module.exports = Router;
