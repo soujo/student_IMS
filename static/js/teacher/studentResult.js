594847ddf0931f4c9ea665555b48b0aa73b1118a
soujanya hazra <soujanya0105@gmail.com>
2022-01-25 17:28:15 +0530

    initial commit

---
+++ static/js/teacher/studentResult.js
@@ -0,0 +1,24 @@
+const dateTime= document.querySelector(".date__time");
+const flagCross=document.querySelector(".flag__cross");
+const flag=document.getElementById("flag");
+const studentResult= document.querySelector(".student__results");
+
+let today=new Date();
+
+let time = today.toLocaleString('en-IN', {  
+    weekday:"long",
+    day:"numeric",
+    month:"long",
+    year:"numeric",
+    hour:"2-digit",
+    hour12:true,
+    minute:"numeric"
+});
+dateTime.innerText=`${time}`;
+
+flagCross.addEventListener("click",function (){
+    flag.classList.remove("flag");
+    flag.classList.add("off");
+});
+
+studentResult.style.backgroundColor = "rgb(0, 60, 128)";
