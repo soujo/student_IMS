b1a4d35687db4f216e8d8df3e7f7c34fd8adeb70
soujanya hazra <soujanya0105@gmail.com>
2022-01-26 13:44:22 +0530

    student attendance data fetched

--- src/route/teacher/T-studentAttendance_route.js
+++ src/route/teacher/T-studentAttendance_route.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const Router = express.Router();
 const TeacherRegister = require("../../models/teacher/teacherRegistration");
+const StudentPersonalInfo = require("../../models/personalInfo");
+const TeacherAllocation = require("../../models/admin/teacherAllocation");
+const TeacherPersonalInfo = require("../../models/teacher/teacherPersonalInfo");
+const studentAttendance = require("../../models/teacher/studentAttendance");
+let regNum;
 
 Router.route("/studentAttendance")
     .get(async (req, res) => {
@@ -9,11 +14,65 @@ Router.route("/studentAttendance")
             const firstName = teacherRegNum?.firstName;
             const lastName = teacherRegNum?.lastName;
 
-        
+            const teachers = await TeacherAllocation.find();
+            const regNumArr = teachers[0]?.regNum;
+            const semArr = teachers[0]?.sem;
+            const subArr = teachers[0]?.sub;
+
+            let index = regNumArr.indexOf(regNum);
+            let semOfTeacher = semArr[index];
+            let subOfTeacher = subArr[index];
+
+            const studentsArr = await StudentPersonalInfo.find({ sem: semOfTeacher });
+
+            let nameArr = new Array;
+            let rollArr = new Array;
+
+            for (let i in studentsArr) {
+                nameArr.push(studentsArr[i].name)
+                rollArr.push(studentsArr[i].roll)
+            }
+            let length = nameArr?.length;
+
+            const students = await studentAttendance?.find({ sem: semOfTeacher, sub: subOfTeacher });
+            let totalClassArr =[];
+            let classAttendedArr = [];
+            let percentageArr=[];
+
+            if(students.length==0){
+                for(let i=0;i<length;i++){
+                    totalClassArr[i]=" ";
+                    classAttendedArr[i]=" ";   
+                    percentageArr[i]=" ";
+
+                }
+            }
+            else{
+                totalClassArr=students[0]?.totalClass;
+                classAttendedArr=students[0]?.classAttended;
+                for(let i=0;i<totalClassArr.length;i++){
+                    percentageArr[i]=(classAttendedArr[i]/totalClassArr[i])*100;
+                }
+            }
+
+            const TPersonalInfo = await TeacherPersonalInfo.findOne({ regNum });
+            const image = TPersonalInfo?.image;
+
+
             const param = {
                 "content": "Student Attendance",
                 "firstName": firstName,
-                "lastName": lastName
+                "lastName": lastName,                
+                "sub": subOfTeacher,
+                "rollArray": rollArr,
+                "nameArray": nameArr,
+                "totalClassArray": totalClassArr,
+                "classAttendedArray": classAttendedArr,
+                "percentageArray":percentageArr,
+                "length": length,
+                "msg": "No student registered",
+                "image":image,
+                "success":req.flash("attendance-success")
             }
 
             res.status(200).render("teacher/T-studentAttendance.pug", param);
