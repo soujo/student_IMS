abba1ae76e9ab2415d3a4ce34b67eff0907a2d2a
soujanya hazra <soujanya0105@gmail.com>
2022-01-28 13:31:47 +0530

    student classmaterials route added

---
+++ src/route/classMaterials_route.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const Router = express.Router();
+const Register = require("../models/userRegistration");
+const PersonalInfo = require("../models/personalInfo");
+const ClassMaterials = require("../models/teacher/studentClassMaterials");
+let materials_1st, materials_2nd, materials_3rd;
+
+Router.route("/classMaterials")
+    .get( async (req, res) => {
+        try {
+
+            const userRoll = await Register.findOne({ roll });
+
+            const firstName = userRoll?.firstName;
+            const lastName = userRoll?.lastName;
+
+            
+            const studentRoll = await PersonalInfo.findOne({ roll });
+            const image = studentRoll?.image;
+            const sem = studentRoll?.sem;
+
+            if (sem == "1st") {
+                materials_1st = await ClassMaterials.find({ sem });
+                materials_2nd = [];
+                materials_3rd = [];
+            }
+            else if (sem == "2nd") {
+                materials_2nd = await ClassMaterials.find({ sem });
+                materials_1st = [];
+                materials_3rd = [];
+            }
+            else {
+                materials_3rd = await ClassMaterials.find({ sem });
+                materials_1st = [];
+                materials_2nd = [];
+            }
+
+            
+            const link = `../static/uploads/classMaterials/`;
+            const param = {
+                "content": "Class Materials",
+                "firstName": firstName,
+                "lastName": lastName,
+                "image": image,
+                "materialArray_1st": materials_1st,
+                "materialArray_2nd": materials_2nd,
+                "materialArray_3rd": materials_3rd,
+                "link": link,
+                "msg": "No class materials from any teacher"
+            };
+            res.status(200).render("classMaterials.pug", param);
+        }
+        catch (err) {
+            res.status(400).send(err);
+            console.log(err);
+        }
+    })
+
+
+module.exports = Router;
