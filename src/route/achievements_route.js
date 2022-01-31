5fc03abc126e780c506fe1c0236b37c0b27a2ed0
soujanya hazra <soujanya0105@gmail.com>
2022-01-31 22:13:24 +0530

    demo clubs fetched

--- src/route/achievements_route.js
+++ src/route/achievements_route.js
@@ -25,17 +25,14 @@ Router.route("/achievements")
             const hobbies = achievementsRoll?.hobbies;
             const sports = achievementsRoll?.sports;
             const language = achievementsRoll?.language;
-            const DSC = achievementsRoll?.clubs[0] == null ? "" : achievementsRoll?.clubs[0].toString();
-            const SAC = achievementsRoll?.clubs[1] == null ? "" : achievementsRoll?.clubs[1].toString();
-            const riyaz = achievementsRoll?.clubs[2] == null ? "" : achievementsRoll?.clubs[2].toString();
-            const elysium = achievementsRoll?.clubs[3] == null ? "" : achievementsRoll?.clubs[3].toString();
-            const chitrank = achievementsRoll?.clubs[4] == null ? "" : achievementsRoll?.clubs[4].toString();
-            const infinitio = achievementsRoll?.clubs[5] == null ? "" : achievementsRoll?.clubs[5].toString();
-            const litmus = achievementsRoll?.clubs[6] == null ? "" : achievementsRoll?.clubs[6].toString();
-            const ecell = achievementsRoll?.clubs[7] == null ? "" : achievementsRoll?.clubs[7].toString();
-            const quiz = achievementsRoll?.clubs[8] == null ? "" : achievementsRoll?.clubs[8].toString();
-            const shutterburg = achievementsRoll?.clubs[9] == null ? "" : achievementsRoll?.clubs[9].toString();
-            const robotics = achievementsRoll?.clubs[10] == null ? "" : achievementsRoll?.clubs[10].toString();
+
+            const club1 = achievementsRoll?.clubs[0] == null ? "" : achievementsRoll?.clubs[0].toString();
+            const club2 = achievementsRoll?.clubs[0] == null ? "" : achievementsRoll?.clubs[0].toString();
+            const club3 = achievementsRoll?.clubs[0] == null ? "" : achievementsRoll?.clubs[0].toString();
+            const club4 = achievementsRoll?.clubs[0] == null ? "" : achievementsRoll?.clubs[0].toString();
+            const club5 = achievementsRoll?.clubs[0] == null ? "" : achievementsRoll?.clubs[0].toString();
+            const club6 = achievementsRoll?.clubs[0] == null ? "" : achievementsRoll?.clubs[0].toString();
+            
 
             const achievement = achievementsRoll?.achievement;
 
@@ -49,7 +46,7 @@ Router.route("/achievements")
                 "hobbies": hobbies,
                 "sports": sports,
                 "language": language,
-                "clubs": [`${DSC} ${SAC} ${riyaz} ${elysium} ${chitrank} ${infinitio} ${litmus} ${ecell} ${quiz} ${shutterburg} ${robotics}`],
+                "clubs": [`${club1} ${club2} ${club3} ${club4} ${club5} ${club6}`],
                 "achievement": achievement,
                 "msg": "No Achievements Added",
                 "success_msg":req.flash("achievementsEdit-success"),
